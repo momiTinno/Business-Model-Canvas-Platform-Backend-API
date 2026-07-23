75398a4bebddf1867165e0e78c265797b944898b
momiTinno <momitul@innovationwithin.com>
2026-07-23 16:54:53 +0600

    fix(auth): remove authentication rate limiting

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,22 +1,17 @@
 import { Router } from "express";
 import { authenticationMiddleware } from "../../middleware/authentication.middleware.js";
-import { authRateLimitMiddleware } from "../../middleware/auth-rate-limit.middleware.js";
 import { authValidationMiddleware } from "./middleware/registration-validation.middleware.js";
 import { AuthController } from "./auth.controller.js";
 export const authRouter = Router();
 const authController = new AuthController();
 authRouter.post(
   "/register",
-  authRateLimitMiddleware.authRateLimit,
   authValidationMiddleware.validateRegistration,
-  authRateLimitMiddleware.emailAuthRateLimit,
   authController.registerUser,
 );
 authRouter.post(
   "/login",
-  authRateLimitMiddleware.authRateLimit,
   authValidationMiddleware.validateLogin,
-  authRateLimitMiddleware.emailAuthRateLimit,
   authController.loginUser,
 );
 authRouter.get(
